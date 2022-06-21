f943973c7d1b4e5a23f7f60f0dd5ab77554515b8
kyanvr <kyan.vanrossem@hotmail.be>
2022-06-21 16:27:06 +0200

    :construction: WIP

--- floppy/flappySnake.py
+++ floppy/flappySnake.py
@@ -12,36 +12,6 @@ pygame.mixer.pre_init(44100, -16, 2, 512)
 mixer.init()
 pygame.init()
 
-# Button
-class Button():
-    def __init__(self, x, y, image, scale):
-        width = image.get_width()
-        height = image.get_height()
-        self.image = pygame.transform.scale(image, (int(width * scale), (int(height * scale))))
-        self.rect = self.image.get_rect()
-        self.rect.topleft = (x, y)
-        self.clicked = False
-
-    def draw(self, surface):
-        action = False
-
-        # get mouse position
-        pos = pygame.mouse.get_pos()
-
-        # check mouseover and clicked conditions
-        if self.rect.collidepoint(pos):
-            if pygame.mouse.get_pressed()[0] == 1 and self.clicked == False:
-                self.clicked = True
-                action = True
-
-        if pygame.mouse.get_pressed()[0] == 0:
-            self.clicked = False
-
-        # draw button on screen
-        surface.blit(self.image, (self.rect.x, self.rect.y))
-
-        return action
-
 # Initialize required elements/environment
 cam = cv.VideoCapture(0)
 window_size = (cam.get(cv.CAP_PROP_FRAME_WIDTH), cam.get(cv.CAP_PROP_FRAME_HEIGHT)) # width by height
@@ -61,14 +31,8 @@ space_between_pipes = 150
 
 # load background music
 pygame.mixer.music.load("./floppy/assets/music/arcade.wav")
-pygame.mixer.music.play(-1, 0, 20000)
 pygame.mixer.music.set_volume(0.3)
-
-# # load button images 
-# exit_img = pygame.image.load("./floppy/assets/exit.png").convert_alpha()
-
-# # create button instances
-# exit_button = Button(100, 200, exit_img, 0.4)   
+pygame.mixer.music.play(-1, 0, 5000)
 
 # Game variables
 game_clock = time.time()
@@ -84,143 +48,147 @@ game_is_running = True
 
 screen.fill((125, 220, 232))
 
-# load button images
-exit_img = pygame.image.load("button_quit.png").convert_alpha()
-
-# create button instance
-exit_button = Button(336, 375, exit_img, 0.4)
-
 def draw_text(text, font, text_col, x, y):
     img = font.render(text, True, text_col)
     screen.blit(img, (x, y))
+
+
+class FlappySnake():
     
-# for a in range(3,0,-1):
-#     text = pygame.font.SysFont("Helvetica Bold.ttf", 64).render(str(a), True, (109, 67, 126))
-#     tr = text.get_rect()
-#     tr.center = (window_size[0]/2, window_size[1]/2)
-#     screen.blit(text, tr)
-#     time.sleep(1)
+    def start():
+        global game_is_running
+        global stage
+        global score
+        global pipe_spawn_timer
+        global time_between_pipe_spawn
+        global game_clock
 
-with mp_face_mesh.FaceMesh(
-    max_num_faces=1,
-    refine_landmarks=True,
-    min_detection_confidence=0.5,
-    min_tracking_confidence=0.5) as face_mesh:
-    while True:
-        # Check if game is running
-        if not game_is_running:
-            screen.fill((238, 194, 255))
-            text = pygame.font.SysFont("Helvetica Bold.ttf", 64).render('Game over!', True, (109, 67, 126))
-            tr = text.get_rect()
-            tr.center = (window_size[0]/2, window_size[1]/2)
-            screen.blit(text, tr)
+        run = True
 
-            # if exit_button.draw(screen):
-            #     pygame.quit()
-            #     sys.exit()
-            
-            # for event in pygame.event.get():
-            #     if event.type == pygame.QUIT:
-            #         pygame.quit()
-            #         sys.exit()
+        with mp_face_mesh.FaceMesh(
+        max_num_faces=1,
+        refine_landmarks=True,
+        min_detection_confidence=0.5,
+        min_tracking_confidence=0.5) as face_mesh:
+            while run:
+                # Check if game is running
+                if not game_is_running:
+                    pygame.mixer.music.fadeout(5000)
+                    screen.fill((238, 194, 255))
+                    text = pygame.font.SysFont("Calibri.ttf", 64).render('Game over!', True, (109, 67, 126))
+                    tr = text.get_rect()
+                    tr.center = (window_size[0]/2, window_size[1]/2)
+                    screen.blit(text, tr)
+                    
+                    pygame.display.update()
+                    pygame.time.wait(2000)
+                    cam.release()
+                    cv.destroyAllWindows()
 
-            pygame.display.update()
-            pygame.time.wait(2000)
-            cam.release()
-            cv.destroyAllWindows()
-            
+                for event in pygame.event.get():
+                        if event.type == pygame.KEYDOWN:
+                            if event.key == pygame.K_ESCAPE:
+                                run = False
+                        if event.type == pygame.QUIT:
+                            run = False
 
-        # Check if user quit window
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                cam.release()
-                cv.destroyAllWindows()
-                pygame.quit()
-                sys.exit()
+                # Check if user quit window
+                for event in pygame.event.get():
+                    if event.type == pygame.QUIT:
+                        cam.release()
+                        cv.destroyAllWindows()
+                        pygame.quit()
+                        sys.exit()
 
-        # Get frame
-        ret, frame = cam.read()
-        if not ret:
-            continue
+                # Get frame
+                ret, frame = cam.read()
+                if not ret:
+                    continue
 
-        # Clear screen
-        screen.fill((125, 220, 232))
+                # Clear screen
+                screen.fill((125, 220, 232))
 
-        # Face mesh
-        frame.flags.writeable = False
-        frame = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
-        results = face_mesh.process(frame)
-        frame.flags.writeable = True
+                # Face mesh
+                frame.flags.writeable = False
+                frame = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
+                results = face_mesh.process(frame)
+                frame.flags.writeable = True
 
-        # Draw mesh
-        if results.multi_face_landmarks and len(results.multi_face_landmarks) > 0:
-            # 94 = Tip of nose
-            marker = results.multi_face_landmarks[0].landmark[94].y
-            bird_frame.centery = (marker - 0.5) * 1.5 * window_size[1] + window_size[1]/2
-            if bird_frame.top < 0: bird_frame.y = 0
-            if bird_frame.bottom > window_size[1]: bird_frame.y = window_size[1] - bird_frame.height
+                # Draw mesh
+                if results.multi_face_landmarks and len(results.multi_face_landmarks) > 0:
+                    # 94 = Tip of nose
+                    marker = results.multi_face_landmarks[0].landmark[94].y
+                    bird_frame.centery = (marker - 0.5) * 1.5 * window_size[1] + window_size[1]/2
+                    if bird_frame.top < 0: bird_frame.y = 0
+                    if bird_frame.bottom > window_size[1]: bird_frame.y = window_size[1] - bird_frame.height
 
-        # Mirror frame, swap axes because opencv != pygame
-        frame = cv.flip(frame, 1).swapaxes(0, 1)
+                # Mirror frame, swap axes because opencv != pygame
+                frame = cv.flip(frame, 1).swapaxes(0, 1)
 
-        # Update pipe positions
-        for pf in pipe_frames:
-            pf[0].x -= pipe_velocity()
-            pf[1].x -= pipe_velocity()
+                # Update pipe positions
+                for pf in pipe_frames:
+                    pf[0].x -= pipe_velocity()
+                    pf[1].x -= pipe_velocity()
 
-        if len(pipe_frames) > 0 and pipe_frames[0][0].right < 0:
-            pipe_frames.popleft()
+                if len(pipe_frames) > 0 and pipe_frames[0][0].right < 0:
+                    pipe_frames.popleft()
 
-        # Update screen
-        pygame.surfarray.blit_array(screen, frame)
-        screen.blit(bird_img, bird_frame)
-        checker = True
-        for pf in pipe_frames:
-            # Check if bird went through to update score
-            if pf[0].left <= bird_frame.x <= pf[0].right:
-                checker = False
-                if not did_update_score:
-                    score += 1
-                    did_update_score = True
-            # Update screen
-            screen.blit(pipe_img, pf[1])
-            screen.blit(pygame.transform.flip(pipe_img, 0, 1), pf[0])
-        if checker: did_update_score = False
+                # Update screen
+                pygame.surfarray.blit_array(screen, frame)
+                screen.blit(bird_img, bird_frame)
+                checker = True
+                for pf in pipe_frames:
+                    # Check if bird went through to update score
+                    if pf[0].left <= bird_frame.x <= pf[0].right:
+                        checker = False
+                        if not did_update_score:
+                            score += 1
+                            did_update_score = True
+                    # Update screen
+                    screen.blit(pipe_img, pf[1])
+                    screen.blit(pygame.transform.flip(pipe_img, 0, 1), pf[0])
+                if checker: did_update_score = False
 
-        # Stage, score text
-        text = pygame.font.SysFont("Helvetica Bold.ttf", 50).render(f'Stage {stage}', True, (255, 255, 255))
-        temp_surface = pygame.Surface(text.get_size())
-        temp_surface.fill((109, 67, 126))
-        temp_surface.blit(text, (0, 0))
-        screen.blit(temp_surface, (50, 50))
-        text = pygame.font.SysFont("Helvetica Bold.ttf", 50).render(f'Score: {score}', True, (255, 255, 255))
-        temp_surface = pygame.Surface(text.get_size())
-        temp_surface.fill((109, 67, 126))
-        temp_surface.blit(text, (0, 0))
-        screen.blit(temp_surface, (50, 100))
+                # Stage, score text
+                text = pygame.font.SysFont("Calibri.ttf", 50).render(f'Stage {stage}', True, (255, 255, 255))
+                temp_surface = pygame.Surface(text.get_size())
+                temp_surface.fill((109, 67, 126))
+                temp_surface.blit(text, (0, 0))
+                screen.blit(temp_surface, (50, 50))
+                text = pygame.font.SysFont("Calibri.ttf", 50).render(f'Score: {score}', True, (255, 255, 255))
+                temp_surface = pygame.Surface(text.get_size())
+                temp_surface.fill((109, 67, 126))
+                temp_surface.blit(text, (0, 0))
+                screen.blit(temp_surface, (50, 100))
 
-        # Update screen
-        pygame.display.flip()
+                # Update screen
+                pygame.display.flip()
 
-        # Check if bird is touching a pipe
-        if any([bird_frame.colliderect(pf[0]) or bird_frame.colliderect(pf[1]) for pf in pipe_frames]):
-            game_is_running = False
+                # Check if bird is touching a pipe
+                if any([bird_frame.colliderect(pf[0]) or bird_frame.colliderect(pf[1]) for pf in pipe_frames]):
+                    game_is_running = False
+                    print(score)
 
-        # Time to add new pipes
-        if pipe_spawn_timer == 0:
-            top = pipe_starting_template.copy()
-            top.x, top.y = window_size[0], random.randint(120 - 1000, window_size[1] - 120 - space_between_pipes - 1000)
-            bottom = pipe_starting_template.copy()
-            bottom.x, bottom.y = window_size[0], top.y + 1000 + space_between_pipes
-            pipe_frames.append([top, bottom])
+                # Time to add new pipes
+                if pipe_spawn_timer == 0:
+                    top = pipe_starting_template.copy()
+                    top.x, top.y = window_size[0], random.randint(120 - 1000, window_size[1] - 120 - space_between_pipes - 1000)
+                    bottom = pipe_starting_template.copy()
+                    bottom.x, bottom.y = window_size[0], top.y + 1000 + space_between_pipes
+                    pipe_frames.append([top, bottom])
 
-        # Update pipe spawn timer - make it cyclical
-        pipe_spawn_timer += 1
-        if pipe_spawn_timer >= time_between_pipe_spawn: pipe_spawn_timer = 0
+                # Update pipe spawn timer - make it cyclical
+                pipe_spawn_timer += 1
+                if pipe_spawn_timer >= time_between_pipe_spawn: pipe_spawn_timer = 0
 
-        # Update stage
-        if time.time() - game_clock >= 10:
-            time_between_pipe_spawn *= 5 / 6
-            stage += 1
-            game_clock = time.time()
+                # Update stage
+                if time.time() - game_clock >= 10:
+                    time_between_pipe_spawn *= 5 / 6
+                    stage += 1
+                    game_clock = time.time()
 
+            pygame.quit()
+            sys.exit()
+
+
+FlappySnake.start()
--- main.py
+++ main.py
@@ -1,19 +1,95 @@
 import pygame
-import pygame_menu
-from snake.main import snakeGame
+from floppy.flappySnake import FlappySnake
 
 pygame.init()
 
-screen = pygame.display.set_mode((600, 400))
+class Button():
+    def __init__(self, x, y, image, scale):
+        width = image.get_width()
+        height = image.get_height()
+        self.image = pygame.transform.scale(image, (int(width * scale), (int(height * scale))))
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (x, y)
+        self.clicked = False
 
-def start_the_game():
-    snakeGame.start()
+    def draw(self, surface):
+        action = False
 
-menu = pygame_menu.Menu('Welcome', 400, 300,
-                       theme=pygame_menu.themes.THEME_BLUE)
+        # get mouse position
+        pos = pygame.mouse.get_pos()
 
-menu.add.text_input('Name :', default='John Doe')
-menu.add.button('Play', start_the_game)
-menu.add.button('Quit', pygame_menu.events.EXIT)
+        # check mouseover and clicked conditions
+        if self.rect.collidepoint(pos):
+            if pygame.mouse.get_pressed()[0] == 1 and self.clicked == False:
+                self.clicked = True
+                action = True
 
-menu.mainloop(screen)
+        if pygame.mouse.get_pressed()[0] == 0:
+            self.clicked = False
+
+        # draw button on screen
+        surface.blit(self.image, (self.rect.x, self.rect.y))
+
+        return action
+
+#create game window
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("Main Menu")
+
+#game variables
+game_running = True
+game_state = "main"
+
+#define fonts
+font = pygame.font.SysFont("calibri", 40)
+
+#define colours
+TEXT_COL = (255, 255, 255)
+
+#load button images
+# resume_img = pygame.image.load("button_resume.png").convert_alpha()
+quit_img = pygame.image.load("button_quit.png").convert_alpha()
+floppy_img = pygame.image.load('button_floppy.png').convert_alpha()
+snake_img = pygame.image.load('button_snake.png').convert_alpha()
+
+#create button instances
+# resume_button = Button(304, 125, resume_img, 0.3)
+quit_button = Button(600, 375, quit_img, 0.3)
+floppy_button = Button(400, 375, floppy_img, 0.3)
+snake_button = Button(200, 375, snake_img, 0.3)
+
+def draw_text(text, font, text_col, x, y):
+  img = font.render(text, True, text_col)
+  screen.blit(img, (x, y))
+
+#game loop
+run = True
+while run:
+
+  screen.fill((52, 78, 91))
+
+  #check if game is paused
+  if game_running == True:
+    #check menu state
+    if game_state == "main":
+      #draw pause screen buttons
+      if floppy_button.draw(screen):
+        game_state = "floppy"
+      if snake_button.draw(screen):
+        game_running = False 
+      if quit_button.draw(screen):
+        run = False
+  if game_state == "floppy":
+    FlappySnake.start()
+
+  #event handler
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+      run = False
+
+  pygame.display.update()
+
+pygame.quit()
